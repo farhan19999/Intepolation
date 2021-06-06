c81a312f96146a35b4d962fc28beff6ee412ae45
farhan19999 <48511298+farhan19999@users.noreply.github.com>
2021-06-06 13:09:08 +0600

    Update LagrangeMethod.py

--- LagrangeMethod.py
+++ LagrangeMethod.py
@@ -15,21 +15,3 @@ def lagrangeMethod(x, n, X, Y):
         #print('L(',i,') = ',l_func(n,i,x,X))
         i+=1
     return ans
-
-if __name__=='__main__':
-    v1 = [22, 25, 27, 30]
-    v2 = [30, 31, 35, 37]
-    p1 = [44, 43, 42, 40]
-    p2 = [40, 35, 30, 25]
-    print('Pressure at V = 28 m^3 = ', lagrangeMethod(28, 3, v1, p1))
-    print('Pressure at V = 32 m^3 = ', lagrangeMethod(32, 3, v2, p2))
-    
-    pol2 = lagrangeMethod(28, 2, v1[1:4], p1[1:4])
-    pol3 = lagrangeMethod(28, 3, v1, p1)
-    error = 100*abs(pol3-pol2)/pol3
-    print('absolute approximate relative error of process 1 : ',error)
-    
-    pol2 = lagrangeMethod(32, 2, v2[:3], p2[:3])
-    pol3 = lagrangeMethod(32, 3, v2, p2)
-    error = 100*abs(pol3-pol2)/pol3
-    print('absolute approximate relative error of process 2 : ',error)
